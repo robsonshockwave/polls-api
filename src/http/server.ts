3249c59ad5d31c6ebd7a52ee3104a9654deeaea7
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-05 23:08:21 -0300

    rota polls

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,27 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
 
 const app = fastify();
 
-app.post('/polls', () => {
-  return 'Hello World!';
+const prisma = new PrismaClient();
+
+app.post('/polls', async (request, reply) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+
+  const { title } = createPollBody.parse(request.body);
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+
+  return reply.status(201).send({
+    pollId: poll.id,
+  });
 });
 
 app.listen({ port: 3333 }).then(() => {

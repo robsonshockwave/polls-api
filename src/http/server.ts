01e2bcc623daa47b48ef3d87264b0182af592a87
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-05 22:26:19 -0300

    configuração inicial

---
+++ src/http/server.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.post('/polls', () => {
+  return 'Hello World!';
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server is running on http://localhost:3333');
+});

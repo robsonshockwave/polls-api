3260c339ea9ffa0ce908d02874a59663e2e5a50b
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-06 23:52:10 -0300

    config cookies e rota vote on poll

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,70 @@
+import { z } from 'zod';
+import { randomUUID } from 'node:crypto';
+import { prisma } from '../../lib/prisma';
+import { FastifyInstance } from 'fastify';
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post('/polls/:pollId/votes', async (request, reply) => {
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid(),
+    });
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid(),
+    });
+
+    const { pollOptionId } = voteOnPollBody.parse(request.body);
+    const { pollId } = voteOnPollParams.parse(request.params);
+
+    let sessionId = request.cookies.sessionId;
+
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          pollId_sessionId: {
+            pollId,
+            sessionId,
+          },
+        },
+      });
+
+      if (
+        userPreviousVoteOnPoll &&
+        userPreviousVoteOnPoll.pollOptionId !== pollOptionId
+      ) {
+        // Apagar o voto anterior
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id,
+          },
+        });
+      } else if (userPreviousVoteOnPoll) {
+        return reply.status(400).send({
+          error: 'User already voted on this poll',
+        });
+      }
+    }
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true,
+        httpOnly: true,
+      });
+    }
+
+    // Criar um novo voto
+    await prisma.vote.create({
+      data: {
+        pollId,
+        pollOptionId,
+        sessionId,
+      },
+    });
+
+    return reply.status(201).send();
+  });
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify';
+import cookie from '@fastify/cookie';
 import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-poll';
+import { voteOnPoll } from './routes/vote-on-poll';
 
 const app = fastify();
 
+app.register(cookie, {
+  secret: 'polls-app-nlw-robsonshockwave', // for cookies signature
+  hook: 'onRequest', // set to false to disable cookie autoparsing or set autoparsing on any of the following hooks: 'onRequest', 'preParsing', 'preHandler', 'preValidation'. default: 'onRequest'
+});
+
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on http://localhost:3333');

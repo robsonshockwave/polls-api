088017cdb606d2c2c7cdb023ad719b4c026e0e98
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-07 22:02:08 -0300

    ranking de votos com redis

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { randomUUID } from 'node:crypto';
 import { prisma } from '../../lib/prisma';
 import { FastifyInstance } from 'fastify';
+import { redis } from '../../lib/redis';
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -38,6 +39,8 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id,
           },
         });
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId);
       } else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({
           error: 'User already voted on this poll',
@@ -64,6 +67,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         sessionId,
       },
     });
+
+    await redis.zincrby(pollId, 1, pollOptionId);
 
     return reply.status(201).send();
   });
